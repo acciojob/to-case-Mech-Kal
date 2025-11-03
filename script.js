3d739a83236be48211326a92c0945b9fade0d46d
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-04 01:22:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
-  // write your code here
+  // If the input string is empty, return '-'
+  if (text === "") return "-";
+  
+  // Return lowercase and uppercase with '-' as the delimiter
+  return text.toLowerCase() + "-" + text.toUpperCase();
 }
 
-// DO not change the code below
-
+// Do not change the code below
 const text = prompt("Enter text:");
 alert(toCase(text));
